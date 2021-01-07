9fa0c080a97199b675e9db33dbe5f5d5e8a63422
Adam Fowler <adamfowler71@gmail.com>
2021-01-07 11:18:54 +0000

    Copyright 2021

--- Sources/SotoS3FileTransfer/S3FileTransferManager+options.swift
+++ Sources/SotoS3FileTransfer/S3FileTransferManager+options.swift
@@ -1,9 +1,16 @@
+//===----------------------------------------------------------------------===//
 //
-//  File.swift
-//  
+// This source file is part of the Soto for AWS open source project
 //
-//  Created by Adam Fowler on 07/11/2020.
+// Copyright (c) 2020-2021 the Soto project authors
+// Licensed under Apache License v2.0
 //
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Soto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 import SotoS3
